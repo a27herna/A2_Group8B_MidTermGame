e071c9b2e8494f6736d20351a5a9b3cc1c8eab37
HoiyaNaranja <a27herna@uwaterloo.ca>
2026-02-27 20:45:13 -0500

    remove or commented previous test code + added level

--- sketch.js
+++ sketch.js
@@ -3,65 +3,57 @@ let gluey;
 let shoulder;
 
 let mainPlayer;
-let floor;
+
+let currentLevel;
 
 function setup() {
-  createCanvas(400, 400);
-
   // ALWAYS ESTABLISH WORLD GRAVITY
   world.gravity.y = 10;
+
+  currentLevel = new Level([]);
+  createCanvas(windowWidth, windowHeight - 200);
 
   mainPlayer = new PlayerBase(width / 2, height / 2, 2);
   const floorThickness = 10;
 
-  let bally = new Sprite(width / 2, 0, 30);
-  bally.color = "orange";
-  bally.overlaps(mainPlayer.mainBody);
+  // let bally = new Sprite(width / 2, 0, 30, 30);
+  // bally.color = "orange";
+  // bally.overlaps(mainPlayer.mainBody);
 
-  floor = new Sprite(
-    width / 2,
-    height - floorThickness / 2,
-    width,
-    floorThickness,
-    KIN,
-  );
-  4;
-
-  mainBody = new Sprite(width, height / 2, 40, 40);
-
-  shoulder = new Sprite(width, height / 2 - mainBody.hh - 15, 40, 10);
-
-  gluey = new WheelJoint(mainBody, shoulder);
-  gluey.damping = 1;
-  gluey.springiness = 0.1;
-  // gluey.range = 100;
+  // floor = new Sprite(
+  //   width / 2,
+  //   height - floorThickness / 2,
+  //   width,
+  //   floorThickness,
+  //   KIN,
+  // );
 }
 
 function draw() {
   background(220);
 
-  if (kb.pressing("q")) {
-    // gluey.speed = 1;
-    // shoulder.rotate(-30);
-    // shoulder.rotation = -30;
-    shoulder.rotateTowards(-30);
-    console.log("hello");
-  } else if (kb.pressing("e")) {
-    shoulder.rotateTowards(30);
-    // shoulder.rotation = 30;
-  } else {
-    shoulder.rotateTowards(0, 0.5);
-    // shoulder.rotation = 0;
-    // shoulder.rotate(0);
-    // gluey.speed = 0;
-  }
+  // if (kb.pressing("q")) {
+  //   // gluey.speed = 1;
+  //   // shoulder.rotate(-30);
+  //   // shoulder.rotation = -30;
+  //   shoulder.rotateTowards(-30);
+  //   console.log("hello");
+  // } else if (kb.pressing("e")) {
+  //   shoulder.rotateTowards(30);
+  //   // shoulder.rotation = 30;
+  // } else {
+  //   shoulder.rotateTowards(0, 0.5);
+  //   // shoulder.rotation = 0;
+  //   // shoulder.rotate(0);
+  //   // gluey.speed = 0;
+  // }
 
-  if (kb.pressing("r")) {
-    console.log(shoulder.rotation);
-  }
-  if (kb.pressing("p")) {
-    bally = new Sprite(width / 2, 0, 30);
-  }
+  // if (kb.pressing("r")) {
+  //   console.log(shoulder.rotation);
+  // }
+  // if (kb.pressing("p")) {
+  //   bally = new Sprite(width / 2, 0, 30, 30);
+  // }
 
   // console.log(shoulder.rotation);
 
