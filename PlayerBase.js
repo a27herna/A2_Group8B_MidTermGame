230dd2cf9aade45c2151e3ac00f520aff6e45d1c
HoiyaNaranja <a27herna@uwaterloo.ca>
2026-02-27 19:54:43 -0500

    impletemented basic movement and floor detection

--- PlayerBase.js
+++ PlayerBase.js
@@ -2,13 +2,15 @@ class PlayerBase {
   constructor(x, y, speed) {
     // What are the basic attributes between players?
     // x,y,
-    this.size = 40;
+    this.size = 60;
     this.speed = speed;
+
+    this.carryonLipHeight = 20;
 
     this.mainBody = new Sprite(x, y, this.size, this.size, DYN);
     this.mainBody.color = "orange";
     this.mainBody.rotationLock = true;
-    this.mainBody.bounciness = 0.05;
+    this.mainBody.bounciness = 0;
     this.mainBody.friction = 0;
 
     this.carryon = new Sprite(
@@ -18,24 +20,35 @@ class PlayerBase {
       this.size * 0.2,
       DYN,
     );
-    this.carryon.addCollider(this.carryon.hw, -this.carryon.hh, 5, 10);
-    this.carryon.addCollider(-this.carryon.hw, -this.carryon.hh, 5, 10);
+    this.carryon.addCollider(
+      this.carryon.hw - 3,
+      -this.carryonLipHeight / 2,
+      6,
+      this.carryonLipHeight,
+    );
+    this.carryon.addCollider(
+      -this.carryon.hw + 3,
+      -this.carryonLipHeight / 2,
+      6,
+      this.carryonLipHeight,
+    );
     this.carryon.rotationLock = true;
 
     this.wheeljoiner = new WheelJoint(this.mainBody, this.carryon);
     this.wheeljoiner.damping = 1;
-    this.wheeljoiner.springiness = 0.01;
+    this.wheeljoiner.springiness = 0.00000000001;
     this.wheeljoiner.visible = false;
 
     this.floorSensor = new Sprite(
       x,
       y + this.mainBody.hh,
       this.size * 0.5,
-      this.size * 0.2,
+      this.size * 0.5,
     );
     this.floorSensor.removeColliders();
     this.floorSensor.visible = true;
     this.floorSensor.mass = 0;
+    this.floorSensor.debug = true;
 
     this.floorJoiner = new GlueJoint(this.mainBody, this.floorSensor);
     this.floorJoiner.visible = false;
@@ -43,6 +56,12 @@ class PlayerBase {
 
   updatePlayer() {
     this.updateInput();
+
+    if (kb.pressing("w") || kb.pressing("space")) {
+      if (this.floorSensor.overlapping(bouncePad)) {
+        this.mainBody.vel.y = -30;
+      }
+    }
   }
 
   updateInput() {
@@ -51,8 +70,8 @@ class PlayerBase {
       (keyIsDown(LEFT_ARROW) || keyIsDown(65));
 
     if (this.floorSensor.overlapping(floor)) {
-      if (kb.presses("w") || kb.presses(" ")) {
-        this.mainBody.vel.y = -5;
+      if (kb.pressing("w") || kb.pressing("space")) {
+        this.mainBody.vel.y = -10;
       }
     }
 
