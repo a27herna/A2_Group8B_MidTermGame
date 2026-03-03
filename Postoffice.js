2bb7befc16258396dc9edf64baaa05f763ee467a
HoiyaNaranja <a27herna@uwaterloo.ca>
2026-03-02 22:08:22 -0500

    attempted to use defaultDraw example but failed

--- Postoffice.js
+++ Postoffice.js
@@ -1,16 +1,31 @@
 class Postoffice {
-    constructor(x, y) {
-        let tempSprite;
+  constructor(x, y) {
+    let tempSprite;
 
-        let height = 90;
-        
-        tempSprite = new Sprite(x, y - height / 2, height/3, height, NONE);
+    let height = 200;
 
-        this.Sprite = tempSprite;
+    tempSprite = new Sprite(x, y - height / 2, height / 1.5, height, "NONE");
+    tempSprite.layer = 0;
 
-    }
+    // let defaultDraw = tempSprite._draw;
 
-    releasePackage() {
-        new Package(this.Sprite.x, this.Sprite.y - this.Sprite.height, "SQUARE", 30, 1);
-    }
+    // tempSprite.draw = function () {
+    //   // add custom code here
+    //   //   rect(-10, -10, 20, 20);
+
+    //   defaultDraw();
+    // };
+
+    this.Sprite = tempSprite;
+  }
+
+  releasePackage() {
+    new Package(
+      this.Sprite.x,
+      this.Sprite.y - this.Sprite.height,
+      "SQUARE",
+      30,
+      1,
+    );
+  }
 }
