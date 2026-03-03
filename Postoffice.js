711b468b7b0a8aab1e34bf8ef05c5c808f58052a
a27herna <a27herna@uwaterloo.ca>
2026-03-03 10:49:17 -0500

    converted to GroupSprite

--- Postoffice.js
+++ Postoffice.js
@@ -29,3 +29,38 @@ class Postoffice {
     );
   }
 }
+
+let PostofficeObj;
+
+function initPostoffice() {
+  let objHeight = 200;
+  let objWidth = objHeight / 1.5
+
+  PostofficeObj = new Group();
+  PostofficeObj.width = objWidth;
+  PostofficeObj.height = objHeight;
+  PostofficeObj.physics = "NONE";
+  // PostofficeObj.offset = {x: 0, y: (-objHeight / 2) + (gridSize / 2) + 2};
+  // PostofficeObj.y = -gridSize / 2;
+  PostofficeObj.layer = 0;
+  // PostofficeObj.debug = true;
+  PostofficeObj.releasePackage = new function() {
+    if (kb.presses('e')) {
+      console.log("special");
+    } 
+  }
+
+  PostofficeObj.overlapping(mainPlayer.mainBody, releasePackage);
+
+  initPackageObj();
+}
+
+function summonPackage() {
+
+  if (kb.presses('e')) {
+    console.log(this.pos);
+
+
+  }
+}
+
